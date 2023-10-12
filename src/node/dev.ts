65df75053a962d28a924c0bfa27766d8423fa260
c0dedance <2627702283@qq.com>
2023-10-12 20:38:34 +0800

    feat: hrm & entry inject

--- src/node/dev.ts
+++ src/node/dev.ts
@@ -1,10 +1,11 @@
 import { createServer as createViteDevServer } from "vite";
+import pluginReact from '@vitejs/plugin-react'
 import { pluginIndexHtml } from "./plugin-r-press/indexHtml";
 
 
 export function createDevServer(root: string = process.cwd()) {
   return createViteDevServer({
     root,
-    plugins: [pluginIndexHtml()]
+    plugins: [pluginIndexHtml(), pluginReact()]
   })
 }
--- src/node/plugin-r-press/indexHtml.ts
+++ src/node/plugin-r-press/indexHtml.ts
@@ -1,6 +1,6 @@
 import { readFile } from "fs/promises";
 import { Plugin } from "vite";
-import { DEFAULT_HTML_PATH } from "../constant";
+import { CLIENT_ENTRY_PATH, DEFAULT_HTML_PATH } from "../constant";
 
 
 export function pluginIndexHtml(): Plugin {
@@ -14,15 +14,31 @@ export function pluginIndexHtml(): Plugin {
           return next();
         }
         const htmlContent = await readFile(DEFAULT_HTML_PATH, 'utf-8');
-
+        const html = await server.transformIndexHtml(req.url, htmlContent, req.originalUrl)
         try {
           res.statusCode = 200;
           res.setHeader("Content-Type", "text/html");
-          res.end(htmlContent);
+          res.end(html);
         } catch (e) {
           return next(e);
         }
       })
-    }
+    },
+    // 入口文件自动注入
+    transformIndexHtml(html) {
+      return {
+        html,
+        tags: [
+          {
+            tag: 'script',
+            attrs: {
+              type: 'module',
+              src: `/@fs/${CLIENT_ENTRY_PATH}`
+            },
+            injectTo: 'body'
+          }
+        ]
+      }
+    },
   }
 }
