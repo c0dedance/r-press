83452c36befffa803e4c8c6ba684a250be1fac5a
c0dedance <2627702283@qq.com>
2023-10-22 14:50:04 +0800

    feat: 集成unocss

--- src/node/vitePlugins.ts
+++ src/node/vitePlugins.ts
@@ -3,6 +3,8 @@ import { pluginIndexHtml } from './plugin-r-press/indexHtml'
 import { pluginConfig } from './plugin-r-press/config'
 import { pluginRoutes } from './plugin-routes'
 import { createPluginMdx } from './plugin-mdx'
+import pluginUnocss from 'unocss/vite'
+import unocssOptions from './unocssOptions'
 import type { SiteConfig } from 'shared/types'
 
 export async function createVitePlugins(
@@ -10,6 +12,7 @@ export async function createVitePlugins(
   restartServer?: () => Promise<void>
 ) {
   return [
+    pluginUnocss(unocssOptions),
     pluginIndexHtml(),
     pluginReact({
       jsxRuntime: 'automatic',
--- src/runtime/theme-default/Layout/index.tsx
+++ src/runtime/theme-default/Layout/index.tsx
@@ -1,10 +1,11 @@
 import { Content } from '../../Content'
+import 'uno.css'
 
 export function Layout() {
   return (
     <div>
       <h1>nav</h1>
-      <aside>sidebar</aside>
+      <aside font="bold">sidebar</aside>
       <aside>
         <Content />
       </aside>
