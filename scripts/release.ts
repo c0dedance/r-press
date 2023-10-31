8a94ac2da5ead4caff11ee763b89a3faccfab3cb
c0dedance <2627702283@qq.com>
2023-11-01 01:11:55 +0800

    fix: release msg

--- scripts/release.ts
+++ scripts/release.ts
@@ -98,7 +98,7 @@ async function main() {
   // 6. 生成 release commit
   step('\nCommitting changes...')
   await run('git', ['add', '-A'])
-  await run('git', ['commit', '-m', `'release: v${targetVersion}'`])
+  await run('git', ['commit', '-m', `release: v${targetVersion}`])
 
   // 7. 执行 npm publish
   step('\nPublishing packages...')
